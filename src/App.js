2b451f11dc8820b136886c8b326f0e500f1d198f
Ivan Carrillo Solache <carrillo.solache@gmail.com>
2022-02-16 23:19:05 -0600

    ref - pokemon component implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 function App() {
   return (
-    <div className="app-contaner">
-        <h1>
-          PokeDex
-        </h1>
-        <a>
-          Find your Pokemon
-        </a>
+    <div className="app-component">
+      <h1> PokeDex</h1>
+      <h2> Find your Pokemon</h2>
+      <div className="pokemon-component">
+        <div className="utterPokemon-component">
+
+        </div>
+        <buttom className="load-more">Load More</buttom>
+      </div>
     </div>
   );
 }
