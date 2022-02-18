dc357a571fe2f99d92e500ec9c1609024a829b5d
Ivan Carrillo Solache <carrillo.solache@gmail.com>
2022-02-17 21:54:01 -0600

    architecting App - pokemon function implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [allPokemon, setAllPokemon] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemon = async () => {
+    const res = await fetch(loadMore)
+    const data =await res.json()
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getAllPokemon()
+  }, [])
+
   return (
     <div className="app-component">
       <h1> PokeDex</h1>
       <h2> Find your Pokemon</h2>
       <div className="pokemon-component">
-        <div className="utterPokemon-component">
+        <div className="all-component">
 
         </div>
         <buttom className="load-more">Load More</buttom>
